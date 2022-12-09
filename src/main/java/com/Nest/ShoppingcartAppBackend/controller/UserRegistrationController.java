17eede981059fe528409061973d3ba90e244c27c
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-09 14:17:11 +0530

    added

--- src/main/java/com/Nest/ShoppingcartAppBackend/controller/UserRegistrationController.java
+++ src/main/java/com/Nest/ShoppingcartAppBackend/controller/UserRegistrationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 
@@ -17,7 +18,7 @@ public class UserRegistrationController {
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/adduser", consumes = "application/json", produces = "application/json")
-    public HashMap<String adduser(@RequestBody UserRegistration u){
+    public String adduser(@RequestBody UserRegistration u){
         System.out.println(u.getName().toString());
         dao.save(u);
         return "user added Successfully";
@@ -30,6 +31,24 @@ public class UserRegistrationController {
         return (List<UserRegistration>) dao.findAll();
     }
 
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userlogin",consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> UserLogin(@RequestBody UserRegistration ul){
+        System.out.println(ul.getEmail());
+        List<UserRegistration> result= (List<UserRegistration>) UserRegistrationDao.FindUserLogin(ul.getEmail(),ul.getPassword());
+        System.out.println(result);
+        HashMap<String,String> st=new HashMap<>();
+        if(result.size()==0) {
+            st.put("status", "failed");
+            st.put("message", "user doesn't exist");
+        }
+        else {
+            int id =result.get(0).getId();
+            st.put("userId",String.valueOf(id));
+            st.put("status", "success");
+            st.put("message", "user added successfully");
+        }
+        return st;
+    }
 
-
-}
+    }
